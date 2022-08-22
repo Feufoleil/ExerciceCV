b11339de8fef4c97359f115fe0257b8ddc4b45c1
Safia TELDJI <feufoleil@MSI.localdomain>
2022-08-22 18:41:37 +0200

    new dlg lines

--- jsfiles/dlg.js
+++ jsfiles/dlg.js
@@ -1,5 +1,11 @@
- document.getElementById("dlgbrd").innerHTML = "Un vent glacial s’empare soudainement de vous, tandis qu’une voix grave émane de l'intérieur du temple." ;
+document.getElementById("dlgtxt").innerHTML = "Un vent glacial s’empare soudainement de vous, tandis qu’une voix grave émane de l'intérieur du temple." ;
+var dlgbuttonclicked = false;
 document.getElementById("dlgbutton").onclick = function() {
-        document.getElementById("dlgbrd").innerHTML = "Cela faisait longtemps que nous n’avions pas eu de visites en ces lieux." ;
-
+        dlgbuttonclicked = !dlgbuttonclicked;
+        if(dlgbuttonclicked) {
+                document.getElementById("dlgtxt").innerHTML = "Cela faisait longtemps que nous n’avions pas eu de visites en ces lieux." ;
+}       else {
+                document.getElementById("dlgtxt").innerHTML = "hmm" ;
+}    
+        
 }
